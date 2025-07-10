f6ed2eefc4a19db8c77990d4b6b6b8069c16410f
Eduardo Lazarini Fontana <contato@eduardofontana.dev>
2025-07-09 22:02:56 -0300

    feat: added swaggert

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { env } from './utils/env-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Fala, Pablo!')
+    .setDescription(
+      'Acho que você deveria passar os três trainees! Não são gente, mas possuem sentimentos! \n\n Amamos você! 💛🖤',
+    )
+    .setVersion('1.0')
+    .addTag('final-project')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap()
